6b01953df443295c5d8adc3aac3bbc267d1fbf62
Aapo Kiiso <aapo@lamia.fi>
2017-05-21 15:22:51 +0300

    Even out some kinks (max status length, larger pool of posts to choose from)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const slack = require('slack');
 const request = require('request');
-const emojiKeywords = require('emojis-keywords');
+const emojiKeywords = Object.keys(require('./emoji-keywords'));
 
 const authToken = process.env.SLACK_TOKEN;
 if (!authToken) {
@@ -22,43 +22,35 @@ Promise.all([getStatusText(), getStatusEmoji()])
     });
 
 async function getStatusText() {
-    let hotPosts = [];
-    try {
-        hotPosts = await new Promise((resolve, reject) => {
-            request('https://www.reddit.com/r/SubredditSimulator/hot.json', (err, res, body) => {
-                if (err) {
-                    return reject(err);
-                }
+    const postsToFind = 50; // Find a nice pool of status text candidates
+    const maxPageNum = 25; // Don't run indefinitely
 
-                let json;
-                try {
-                    json = JSON.parse(body);
-                } catch (e) {
-                    return reject(e);
-                }
+    let hotTextPosts = [];
+    let pageNum = 1;
+    while (pageNum < maxPageNum || hotTextPosts.length < postsToFind) {
+        let pagePosts = await getHotPosts(pageNum);
 
-                return resolve(json);
+        // Remove cruft
+        pagePosts = pagePosts['data']['children'];
+        pagePosts = pagePosts.map(hotPost => hotPost.data);
+
+        let pageTextPosts = pagePosts
+            .filter(post => !post['stickied']) // Ignore stickied posts
+            .filter(post => post['selftext'] && post['selftext'].length) // Ignore image posts (no context in Slack)
+            .filter(post => post['title'].length < 100); // Slack status text max length is 100 chars
+
+        // Optional subreddit-simulator filtering
+        const subreddit = process.env.SUBREDDIT;
+        if (subreddit) {
+            const regex = new RegExp('^' + subreddit + '[_-]+SS$', 'i');
+            pageTextPosts = pageTextPosts.filter(post => {
+                return post.author.match(regex);
             });
-        });
-    } catch (e) {
-        console.error(e);
-    }
+        }
 
-    // Remove cruft
-    hotPosts = hotPosts['data']['children'];
-    hotPosts = hotPosts.map(hotPost => hotPost.data);
+        hotTextPosts = hotTextPosts.concat(pageTextPosts);
 
-    let hotTextPosts = hotPosts
-        .filter(post => !post['stickied']) // Ignore stickied posts
-        .filter(post => post['selftext'] && post['selftext'].length); // Ignore image posts (no context in Slack)
-
-    // Optional subreddit filtering
-    const subreddit = process.env.SUBREDDIT;
-    if (subreddit) {
-        const regex = new RegExp('^' + subreddit + '[_-]+SS$', 'i');
-        hotTextPosts = hotTextPosts.filter(post => {
-            return post.author.match(regex);
-        });
+        pageNum++;
     }
 
     // Select random post
@@ -68,7 +60,37 @@ async function getStatusText() {
         hotTextPost = hotTextPosts[i];
     }
 
-    return hotTextPost ? hotTextPost['title'] : '';
+    const statusText = hotTextPost ? hotTextPost['title'] : '';
+
+    return statusText;
+}
+
+async function getHotPosts(page) {
+    let hotPosts = [];
+
+    try {
+        hotPosts = await new Promise((resolve, reject) => {
+            const offset = page * 25; // Each result page is 25 items
+
+            request({
+                url: 'https://www.reddit.com/r/SubredditSimulator/hot.json',
+                qs: {
+                    count: offset // Reddit confusingly labels offset as count in URLs
+                },
+                json: true
+            }, (err, res, json) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                return resolve(json);
+            });
+        });
+    } catch (e) {
+        console.error(e);
+    }
+
+    return hotPosts;
 }
 
 async function getStatusEmoji() {
